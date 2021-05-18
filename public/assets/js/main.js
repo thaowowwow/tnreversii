5002bf65cf687add089d36311ce41b1470f00eb8
Thao Nguyen <thachn1@uci.edu>
2021-05-18 07:48:04 -0700

    chat feature

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -6,17 +6,72 @@ function getIRIParameterValue(requestedKey){
         let key = data[0];
         let value = data[1];
         if (key === requestedKey){
-            return value;
-
+        return value;
         }
     }    
 }
 
-let username = getIRIParameterValue('username');
+let username = decodeURI(getIRIParameterValue('username'));
 if ((typeof username == 'undefined') || (username == null)){
     username = "Anonymous_"+Math.floor(Math.random()*1000);
+}
+
+let chatRoom = 'Lobby';
+
+/* Set up the socket.io connection to the server */
+let socket = io();
+socket.on('log',function(array) {
+    console.log.apply(console,array);
+});
+
+socket.on('join_room_response', (payload) =>{
+    if(( typeof payLoad == 'undefined') || (payload === null)){
+        console.log('Server did not send a payload');
+        return;
+    }
+    if(payload.result === 'fail'){
+        console.log(payload.message);
+        return;
+    }
+    let newString = '<p class= \'join_room_response\'> '+payload.username+' joined the '+payload.room+'. (There are '+payload.count+' users in this room)</p>';
+    $('#messages').prepend(newString);
+    
+});
+
+function sendChatMessage(){
+    let request = {};
+    request.room = chatRoom;
+    request.username = username;
+    request.message = $('#chatMessage').val();
+    console.log('**** Client log message, sending \'send_chat_message\' command: '+JSON.stringify(request));
+    socket.emit('send_chat_message',request);
 
 }
 
-$('#messages').prepend('<b>'+username+':<b>');
+
+
+socket.on('send_chat_message_response', (payload) => {
+    if(( typeof payLoad == 'undefined') || (payload === null)){
+        console.log('Server did not send a payload');
+        return;
+    }
+    if(payload.result === 'fail'){
+        console.log(payload.message);
+        return;
+    }
+    let newString = '<p class=\'chat_message\'><b>'+payload.username+'</b>: '+payload.message+'</p>';
+    $('#messages').prepend(newString);
+    
+});
+
+
+/* Request to join the chat room */
+$( () => {
+    let request = {};
+    request.room = chatRoom;
+    request.username = username;
+    console.log('**** Client log message, sending \'join_room\' command: '+JSON.stringify(request));
+    socket.emit('join_room',request);
+});
+
 
